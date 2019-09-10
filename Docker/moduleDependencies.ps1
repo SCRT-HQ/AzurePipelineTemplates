4b1d319e911cd7de9d7d0eb1a3db5d3b5e0be2f5
Nate Ferrell <nate@scrthq.com>
2019-09-10 12:22:15 -0500

    updated build and test process

---
+++ Docker/moduleDependencies.ps1
@@ -0,0 +1,9 @@
+@{
+    Configuration     = '1.3.1'
+    PackageManagement = '1.3.1'
+    PowerShellGet     = '2.1.2'
+    InvokeBuild       = '5.5.2'
+    psake             = '4.8.0'
+    Pester            = '4.9.0'
+    PSScriptAnalyzer  = '1.18.2'
+}
