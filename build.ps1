847087933d6886986a0d968f7c4534b8abd863c7
Nate Ferrell <nate@scrthq.com>
2019-09-10 11:40:40 -0500

    updated container test script

--- build.ps1
+++ build.ps1
@@ -1,12 +1,29 @@
 Param ($Task)
 
 try {
-    Write-Host "Executing task: $Task`n`nModules installed:"
-    Get-Module -ListAvailable
+    Write-Host "Executing task: $Task"
     Write-Host "`n`nPSVersionTable:"
     [PSCustomObject]$PSVersionTable
     Write-Host "GitVersion version:`n"
     'GitVersion ' + (gitversion /version)
+    Write-Host "`n`nValidating Modules installed:"
+    @{
+        Configuration     = '1.3.1'
+        PackageManagement = '1.3.1'
+        PowerShellGet     = '2.1.2'
+        InvokeBuild       = '5.5.2'
+        psake             = '4.8.0'
+        Pester            = '4.9.0'
+        PSScriptAnalyzer  = '1.18.2'
+    }.GetEnumerator() | ForEach-Object {
+        $module = $_.Key
+        $version = $_.Value
+        Write-Host "Checking if module $($module)@$($version)+ is installed"
+        if ($null -eq (Get-Module $_.Key -ListAvailable | Where-Object {$_.Version -ge [version]$version})) {
+            throw "$($module)@$($version)+ was not found on this container!"
+            exit 1
+        }
+    }
 }
 catch {
     throw $_
